ebd2d9f5cdd904c6dbed2b4189b74feb15ffc882
nothing36 <prodipsarker953@gmail.com>
2025-10-28 13:17:39 +0600

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+const loadCustomerDetails = () => {
+  url = "https://dummyjson.com/carts";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCustomerDetails(data.carts));
+};
+// "total": 103774.85,
+//       "discountedTotal": 89686.65,
+//       "userId": 33,
+//       "totalProducts": 4,
+//       "totalQuantity": 15
+
+// display customer details
+const displayCustomerDetails = (carts) => {
+  // console.log(carts);
+  const customer_details = document.getElementById("customer-datails");
+  carts.forEach((id) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+          <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img class="w-40"
+      src="./resources/image.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Customer- ${id.id}</h2>
+    <h3>Total Products: ${id.totalProducts}</h3>
+ <h3>Total Quantity: ${id.totalQuantity}</h3>
+ <h3 class="text-sm">Total: ${id.total}</h3>
+ <h2 class="font-bold text-base">Discounted Total: ${id.discountedTotal}</h2>
+    <div class="card-actions justify-center">
+      <button class="btn btn-primary">View Details</button>
+    </div>
+  </div>
+            </div>
+        
+        `;
+    customer_details.appendChild(div);
+  });
+};
+
+loadCustomerDetails();
